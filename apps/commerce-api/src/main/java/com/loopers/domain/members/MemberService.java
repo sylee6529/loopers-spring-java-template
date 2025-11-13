d1c961c91cc24faae5af85ff1d4e4a496376a470
Seoyeon Lee <68765200+sylee6529@users.noreply.github.com>
2025-11-13 13:00:28 +0900

    refactor: 중복 회원 경쟁적 생성 시 유니크 제약조건 에러 처리

--- apps/commerce-api/src/main/java/com/loopers/domain/members/MemberService.java
+++ apps/commerce-api/src/main/java/com/loopers/domain/members/MemberService.java
@@ -3,6 +3,7 @@ package com.loopers.domain.members;
 import com.loopers.support.error.CoreException;
 import com.loopers.support.error.ErrorType;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,16 +17,17 @@ public class MemberService {
 
     @Transactional
     public MemberModel registerMember(String memberId, String email, String password, String birthDate, Gender gender) {
-        if (memberRepository.existsByMemberId(memberId)) {
+        String encodedPassword = passwordEncoder.encode(password);
+        MemberModel member = new MemberModel(memberId, email, encodedPassword, birthDate, gender);
+
+        try {
+            return memberRepository.save(member);
+        } catch (DataIntegrityViolationException e) {
             throw new CoreException(
                     ErrorType.CONFLICT,
                     "이미 가입된 ID 입니다."
             );
         }
-
-        String encodedPassword = passwordEncoder.encode(password);
-        MemberModel member = new MemberModel(memberId, email, encodedPassword, birthDate, gender);
-        return memberRepository.save(member);
     }
 
     @Transactional(readOnly = true)
